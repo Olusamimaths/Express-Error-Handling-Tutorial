8620c08f1691441f2b28753f3fed38d5a609df66
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2020-01-10 22:10:59 +0100

    Finished

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res, next) => {
+  throw new Error("Something went wrong!");
   res.send("Welcome to main route!");
 });
 
 app.get("/about", (req, res, next) => {
   res.send("This is the about route!");
 });
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+// error handler middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+      error: {
+        status: error.status || 500,
+        message: error.message || 'Internal Server Error',
+      },
+    });
+  });
 
 app.listen(port, () => console.log(`App listening on port: ${port}`));
